80b6994332112cb077d9b1f4463139e060c22f6b
muhammod1 <muhammodholawale@gmail.com>
2023-09-17 00:08:43 +0100

    responsiveness changes

--- client/src/app/launchpad/page.tsx
+++ client/src/app/launchpad/page.tsx
@@ -1,14 +1,27 @@
+'use client'
+
+import { useState } from 'react'
 import Button from "@/common/Button";
 import SideBar from "@/common/navs/side/SideNavigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import Apply from "./apply/page";
 import NavTools from "@/common/navs/NavTools";
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 const LaunchPad = () => {
+  const [Open, setOpen] = useState(true)
+
+  const menuNav = () => {
+    setOpen(!Open);
+  };
   return (
     <div className="flex flex-row gap-10  min-h-screen">
       <div className="w-[12rem] h-screen">
+      <div className="cursor-pointer" onClick={menuNav}>
+          <AiOutlineMenu className="tablet_l:hidden flex h-7 w-7 items-center" />
+        </div>
         <SideBar />
       </div>
       <div className="mr-auto w-[80%] py-10">
