d8c58d184008bff5db8c8f1cd741a6804f9a9841
muhammod1 <muhammodholawale@gmail.com>
2023-09-17 17:23:40 +0100

    adjustment

--- client/src/components/ColletionHeader.tsx
+++ client/src/components/ColletionHeader.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 
 const ColletionHeader = () => {
   return (
-    <div className='my-32 w-full tablet_l:w-[97%] laptop_l:w-[97%] max-w-[1280px]'>
-      <div className='relative z-10 flex justify-center items-center mx-auto w-full h-[400px] laptop:h-[500px] rounded-lg'>
+    <div className='mt-32 mb-24 w-full tablet_l:w-[97%] laptop_l:w-[97%] max-w-[1280px]'>
+      <div className='relative z-10 flex justify-center items-center mx-auto w-full h-[500px] laptop:h-[500px] rounded-lg'>
         <Image 
-        className='hidden tablet_l:block absolute  w-full'
+        className='hidden tablet_l:block h-[450px] absolute  w-full'
           src='/collection.png'
           width={1200}
           height={1000}
