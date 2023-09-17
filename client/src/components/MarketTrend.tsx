025640daa0f0bf4e82836f0a9882944c80de789e
muhammod1 <muhammodholawale@gmail.com>
2023-09-17 17:57:44 +0100

    adjustment

--- client/src/components/MarketTrend.tsx
+++ client/src/components/MarketTrend.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { orbitron } from "@/fonts/fonts";
+import React from "react";
 
 const MarketTrend = () => {
   return (
-    <div>MarketTrend</div>
-  )
-}
+    <div className={`${orbitron.className} my-28`}>
+      <div className="mx-auto w-[95%] laptop_l:w-[920px]">
+        <h2 className="text-2xl mb-3">Market Trend</h2>
+        <div className="border-[1px] border-[#FFC72C] rounded-lg">
 
-export default MarketTrend
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketTrend;
